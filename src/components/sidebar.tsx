e9584ceaf3ae48d5ba68959b9ae23dbcbc254105
kristiandavidd <kristiandavid644@gmail.com>
2023-07-07 14:31:29 +0700

    fix bug

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -5,12 +5,14 @@ import { useRouter } from "next/router";
 export default function Sidebar() {
     const router = useRouter();
     return(
-    <Link href="/dashboard" className="w-[150px] h-screen bg-center bg-cover rounded-r-[32px] flex flex-col items-center gap-4 p-5" 
+    <div className="w-[150px] h-screen bg-center bg-cover rounded-r-[32px] flex flex-col items-center gap-4 p-5" 
         style={{
             backgroundImage: "url('../../bg.svg')",
         }}> 
 
-        <img src="../../logos.svg" alt="" className="w-[40px]"/>
+        <Link href="/dashboard">
+            <img src="../../logos.svg" alt="" className="w-[40px]"/>    
+        </Link>
         <Link href="/dashboard" className={`hover:bg-pantau-light-green/60 w-[40px] h-[40px] flex justify-center items-center rounded-lg active:bg-pantau-light-green/60 ${router.asPath === "/dashboard" && 'bg-pantau-light-green'}`} >
             <Home 
                 size={24}
@@ -34,6 +36,6 @@ export default function Sidebar() {
                 color={'white'}
             />
         </Link>
-    </Link>
+    </div>
     )
 }
