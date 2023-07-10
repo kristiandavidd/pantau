a665e5c4fb2ca0ae577752913924d4401a0dd02d
kurzzt <87642181+kurzzt@users.noreply.github.com>
2023-07-11 02:24:12 +0700

    bug fixed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { AuthProvider } from '@/firebase/AuthProvider'
+// import { AuthProvider } from '@/firebase/AuthProvider'
 import { MantineProvider } from "@mantine/core";
 import { CookiesProvider } from 'react-cookie';
 
 export default function App({ Component, pageProps }: AppProps) {
   return( 
-  <AuthProvider>
+  // <AuthProvider>
     <CookiesProvider>
       <MantineProvider withNormalizeCSS withGlobalStyles>
         <Component {...pageProps} />
       </MantineProvider>
     </CookiesProvider>
-  </AuthProvider>
+  // </AuthProvider>
   )
 }
